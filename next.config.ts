7b8d00ffa53920a6f86a562dc9bea1437b76a88e
Mobo10 <jkmuogboh@gmail.com>
2025-07-07 09:30:32 +0100

    initial commit

---
+++ next.config.ts
@@ -0,0 +1,14 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: "9hfjy94et2.ufs.sh",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
