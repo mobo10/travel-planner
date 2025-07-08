74838f02d3c4851b84ca745426d24ac4a3934819
Jeffrey Muogboh <98775629+mobo10@users.noreply.github.com>
2025-07-08 14:29:43 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  /* config options here */
   images: {
     remotePatterns: [
       {
         hostname: "9hfjy94et2.ufs.sh",
       },
     ],
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   },
 };
 
